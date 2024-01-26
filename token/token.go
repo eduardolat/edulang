72edd2b804f899b77af7dceac231cc83302edc9a
Luis Eduardo Jeréz Girón <eduardo.devop@gmail.com>
2024-01-26 15:11:35 -0600

    Add keyword lookup functionality

--- token/token.go
+++ token/token.go
@@ -46,3 +46,16 @@ const (
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
 )
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+// LookupIdent returns the TokenType for a given identifier.
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
